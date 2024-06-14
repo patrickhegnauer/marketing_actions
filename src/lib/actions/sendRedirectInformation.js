322220a16d4ce97277a3ad70299edb8f9c5f49cb
Patrick Hegnauer <patrick.hegnauer@bluewin.ch>
2024-06-14 09:06:40 +0200

    v1.0.9

--- src/lib/actions/sendRedirectInformation.js
+++ src/lib/actions/sendRedirectInformation.js
@@ -4,8 +4,8 @@ module.exports = function(settings,event) {
   try {
     var chash = _satellite.getVar('DataLayer - user.userInfo.chash') != null ? _satellite.getVar('DataLayer - user.userInfo.chash').toUpperCase() : null;
     var redirectURL = event.element.href;
-    target == 'external' ? (redirectURL.indexOf('?') > -1 ? redirectURL = redirectURL+'&chash='+chash : redirectURL = redirectURL+'?chash='+chash) : redirectURL = redirectURL;
     var target = redirectURL.indexOf('https://') > -1 ? 'external' : 'internal';
+    target == 'external' ? (redirectURL.indexOf('?') > -1 ? redirectURL = redirectURL+'&chash='+chash : redirectURL = redirectURL+'?chash='+chash) : redirectURL = redirectURL;
     const teaserEvent = new CustomEvent('event-action-teaser', {
       detail: {
         link: redirectURL,
